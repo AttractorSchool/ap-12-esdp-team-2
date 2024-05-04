487db35dab2c67a97030b73526887c40a5cf25c8
Yerbolat Karzhaubay <karzhaubayerbolat@gmail.com>
2024-05-04 11:53:18 +0500

    Add app API

--- unitysphere/core/settings.py
+++ unitysphere/core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
--- unitysphere/core/urls.py
+++ unitysphere/core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('account/', include('accounts.urls')),
+
+    path('api/v1/', include('api.urls')),
 ]
