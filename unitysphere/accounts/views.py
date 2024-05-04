0fd2d3169aa056c70f3ada82362b7fe68ab3b1ad
Yerbolat Karzhaubay <karzhaubayerbolat@gmail.com>
2024-05-04 11:45:38 +0500

    Write sign up view and build template

--- unitysphere/accounts/views.py
+++ unitysphere/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from accounts.forms import RegisterUserForm
+
+
+class RegisterUserView(generic.CreateView):
+    form_class = RegisterUserForm
+    template_name = 'accounts/register_user.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['page_title'] = 'Регистрация'
+        return ctx
