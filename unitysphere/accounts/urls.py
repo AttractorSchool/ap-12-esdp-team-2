d468011a0518f586d6cdcd4f1f286c9f912f8993
Yerbolat Karzhaubay <karzhaubayerbolat@gmail.com>
2024-05-06 14:46:59 +0500

    Add basic logout view

--- unitysphere/accounts/urls.py
+++ unitysphere/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from accounts import views
@@ -5,4 +6,5 @@ from accounts import views
 urlpatterns = [
     path('register/', views.RegisterUserView.as_view(), name='register'),
     path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
