158dee4ec90eba1d79f1ac11416dda4010e84129
gaukharmu <goowwaa@gmail.com>
2024-05-08 18:00:34 +0500

    registering models in the admin panel

--- unitysphere/clubs/admin.py
+++ unitysphere/clubs/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Club, ClubCategory, City 
 
-# Register your models here.
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'category', 'logo',
+        'managers', 'description', 
+        'email', 'phone', 'city', 
+        'address', 'is_active',
+    )
+
+
+@admin.register(ClubCategory)
+class ClubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'is_active',
+    )
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'iata_code',
+    )
