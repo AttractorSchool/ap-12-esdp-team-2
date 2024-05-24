05052c80f9695a3047f450fe1d2a9ae690f136bf
Yerbolat Karzhaubay <karzhaubayerbolat@gmail.com>
2024-05-24 17:57:09 +0500

    Small refactoring and add documentation for permission classes

--- unitysphere/clubs/mixins.py
+++ unitysphere/clubs/mixins.py
@@ -1,9 +1,37 @@
 
 
 class ClubActionSerializerMixin:
+    """
+    Миксин, который позволяет использовать разные сериализаторы для разных действий в ViewSet-ах.
+
+    Этот миксин позволяет определять разные сериализаторы для различных действий (например, 'create', 'update',
+    'retrieve', и т. д.) внутри ViewSet-ов. Для этого необходимо создать атрибут класса ACTION_SERIALIZERS,
+    который сопоставляет действия с соответствующими сериализаторами.
+
+    Атрибуты:
+        ACTION_SERIALIZERS (dict): Словарь, который сопоставляет действия с соответствующими сериализаторами.
+
+    Пример использования:
+        class MyViewSet(ClubActionSerializerMixin, viewsets.ModelViewSet):
+            ACTION_SERIALIZERS = {
+                'create': MyCreateSerializer,
+                'update': MyUpdateSerializer,
+                'retrieve': MyRetrieveSerializer,
+            }
+    """
+
     ACTION_SERIALIZERS = {}
 
     def get_serializer_class(self):
+        """
+        Возвращает класс сериализатора для текущего действия.
+
+        Если для текущего действия определен соответствующий сериализатор в атрибуте ACTION_SERIALIZERS,
+        возвращается этот сериализатор. В противном случае используется сериализатор, определенный в родительском классе.
+
+        Возвращает:
+            Serializer: Класс сериализатора для текущего действия.
+        """
         if serializer := self.ACTION_SERIALIZERS.get(self.action):
             return serializer
         return super().get_serializer_class()
--- unitysphere/clubs/permissions.py
+++ unitysphere/clubs/permissions.py
@@ -2,23 +2,39 @@ from rest_framework import permissions
 
 
 class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение, позволяющее только аутентифицированным пользователям выполнять операции записи.
+
+    Если запрос имеет метод только для чтения (GET, HEAD, OPTIONS), разрешение будет предоставлено.
+    В противном случае разрешение будет предоставлено только аутентифицированным пользователям.
+    """
     def has_permission(self, request, view):
-        if request.method not in permissions.SAFE_METHODS:
-            return request.user.is_authenticated
-        return True
+        return (
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_authenticated
+        )
 
 
 class ClubPermission(IsAuthenticatedOrReadOnly):
+    """
+    Разрешение для операций над объектами клубов.
 
+    Разрешает только менеджерам клуба выполнять операции записи (удаление, обновление, частичное обновление).
+    """
     def has_object_permission(self, request, view, obj):
-        if view.action in ['destroy', 'update', 'partial_update']:
-            return request.user in obj.managers.all()
+        if view.action in ('destroy', 'update', 'partial_update'):
+            return obj.managers.filter(id=request.user.id).exists()
         return True
 
 
 class ClubObjectsPermission(IsAuthenticatedOrReadOnly):
+    """
+    Разрешение для операций над объектами, относящимися к клубу.
 
+    Разрешает только менеджерам клуба выполнять операции записи (удаление, обновление, частичное обновление)
+    над объектами, связанными с клубом.
+    """
     def has_object_permission(self, request, view, obj):
-        if view.action in ['destroy', 'update', 'partial_update']:
-            return request.user in obj.club.managers.all()
+        if view.action in ('destroy', 'update', 'partial_update'):
+            return request.user in obj.club.managers.filter(id=request.user.id).exists()
         return True
