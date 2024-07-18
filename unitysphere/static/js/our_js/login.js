86b94112ba090a74423a15253ecb54a04f8285b3
gaukharmu <goowwaa@gmail.com>
2024-07-18 22:55:18 +0500

    small refactoring on js file

--- unitysphere/static/js/our_js/login.js
+++ unitysphere/static/js/our_js/login.js
@@ -1,15 +1,19 @@
+let baseURL = '127.0.0.1:8000';
+
 $('#login_user_form').submit(function (e) {
+    e.preventDefault();
     let login_form = document.getElementById("login_user_form") ;
     let login = $("#id_username").val();
     let password = $("#id_password2").val();
     $.ajax({
         type: "post",
-        url: 'http://127.0.0.1:8000/api/v1/login/',
+        url: `http://${baseURL}/api/v1/login/`,
         data: JSON.stringify({username: login, password: password}),
         dataType: 'json',
         contentType: 'application/json',
         success: function (response) {
             localStorage.setItem('apiToken', response.token);
+            login_form.submit();
         },
         error: function(response, status){console.log(response, status);}
     });
